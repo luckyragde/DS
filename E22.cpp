db7ea4b5bf2194b81e2031195f73117dc728a4c1
luckyragde <lragade2004@gmail.com>
2025-05-07 21:31:50 +0530

    Update E22.cpp

--- E22.cpp
+++ E22.cpp
@@ -1,33 +1,80 @@
 #include <iostream>
-#include <vector>
-#include <queue> // For priority_queue
 using namespace std;
+
+const int MAX = 100;
+
+class Heap {
+    int arr[MAX];
+    int size;
+    bool isMax; // true for max-heap, false for min-heap
+
+public:
+    Heap(bool type) {
+        size = 0;
+        isMax = type;
+    }
+
+    void insert(int value) {
+        if (size >= MAX) {
+            cout << "Heap Overflow!\n";
+            return;
+        }
+
+        arr[size] = value;
+        int i = size;
+        size++;
+
+        // Bubble up
+        while (i > 0) {
+            int parent = (i - 1) / 2;
+            if ((isMax && arr[i] > arr[parent]) || (!isMax && arr[i] < arr[parent])) {
+                swap(arr[i], arr[parent]);
+                i = parent;
+            } else {
+                break;
+            }
+        }
+    }
+
+    int getTop() {
+        if (size == 0) {
+            cout << "Heap is empty.\n";
+            return -1;
+        }
+        return arr[0];
+    }
+
+    void display() {
+        cout << (isMax ? "Max" : "Min") << "-Heap: ";
+        for (int i = 0; i < size; ++i) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+};
 
 int main() {
     int n;
     cout << "Enter number of students: ";
     cin >> n;
 
-    vector<int> marks(n);
-    cout << "Enter marks of " << n << " students:\n";
-    for (int i = 0; i < n; ++i) {
-        cin >> marks[i];
+    Heap maxHeap(true);
+    Heap minHeap(false);
+
+    for (int i = 0; i < n; i++) {
+        int mark;
+        cout << "Enter mark of student " << (i + 1) << ": ";
+        cin >> mark;
+        maxHeap.insert(mark);
+        minHeap.insert(mark);
     }
 
-    priority_queue<int> maxHeap;
-    priority_queue<int, vector<int>, greater<int> > minHeap;
+    cout << "\n";
+    maxHeap.display();
+    minHeap.display();
 
-    for (int i = 0; i < n; ++i) {
-        maxHeap.push(marks[i]);
-        minHeap.push(marks[i]);
-    }
+    cout << "\nMaximum Marks: " << maxHeap.getTop() << "\n";
+    cout << "Minimum Marks: " << minHeap.getTop() << "\n";
 
-    int maxMark = maxHeap.top();
-    int minMark = minHeap.top();
-
-    cout << "Maximum mark = " << maxMark << endl;
-    cout << "Minimum mark = " << minMark << endl;
-
-    return 0;
+    return 0;
 }
-
